65ea02697c537da21f9d351a359119499d7933e4
GONZO ART <210776491+GonzoArtPrint@users.noreply.github.com>
2025-05-30 01:36:55 +0200

    solve getComputerChoice

---
+++ rps.js
@@ -0,0 +1,10 @@
+let x = Math.floor(Math.random()*3);
+
+function getComputerChoice(x) { 
+
+    if (x == 1) { alert ("ROCK")};
+    if (x == 2) { alert ("PAPER")};
+    if (x == 3) { alert ("SCISSORS")};
+    }
+    
+    getComputerChoice(x)
