18bf2be81c27be3c846a761471fafb49d5956280
GONZO ART <210776491+GonzoArtPrint@users.noreply.github.com>
2025-05-30 10:58:00 +0200

    solve getHumanChoice

--- rps.js
+++ rps.js
@@ -1,10 +1,17 @@
+
+
+function getHumanChoice () { let userAnswer = prompt ("Please choose: ROCK, PAPER or SCISSORS");
+    return userAnswer;
+}
+getHumanChoice()
+
 let x = Math.floor(Math.random()*3);
 
 function getComputerChoice(x) { 
 
-    if (x == 1) { alert ("ROCK")};
-    if (x == 2) { alert ("PAPER")};
-    if (x == 3) { alert ("SCISSORS")};
+    if (x == 0) { alert ("ROCK")};
+    if (x == 1) { alert ("PAPER")};
+    if (x == 2) { alert ("SCISSORS")};
     }
     
     getComputerChoice(x)
